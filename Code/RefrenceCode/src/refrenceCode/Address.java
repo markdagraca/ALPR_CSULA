2082b47b8c335630c6e8cc901af62f75d81d5e78
YijieLiX <loveleongjingru@gmail.com>
2018-11-08 14:16:14 -0800

    Update Code/RefrenceCode/src/refrenceCode/Address.java

--- Code/RefrenceCode/src/refrenceCode/Address.java
+++ Code/RefrenceCode/src/refrenceCode/Address.java
@@ -1,19 +1,47 @@
 package refrenceCode;
 
 public class Address {
-    private String street,
+	private String street,
     city;
-    /**
-     * Zip Code -1 means no ZipCode present
-     */
-    private int zipcode;
+    private String zipcode;
     private States state;
+    
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/licensedata";
+    
+    private static final String USER = "root";
+    private static final String PASS = "a931019555";
 
-    public Address() {
-        street=city="";
-        zipcode=-1;
-        state=null;
+    public Address(String id) {
+    	Connection conn = null;
+        Statement stmt = null;
+    	
+        try{
+            Class.forName(JDBC_DRIVER);
 
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM address WHERE license_number = '" + id + "'";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()) {           	
+            	this.street = rs.getString("address");
+            	this.city = rs.getString("city");
+            	String states = rs.getString("region");
+            	this.state  = state.valueOf(states);
+            	this.zipcode = rs.getString("postalcode");
+            	
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            se.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public String getStreet() {
@@ -32,11 +60,11 @@ public class Address {
         this.city = city;
     }
 
-    public int getZipcode() {
+    public String getZipcode() {
         return zipcode;
     }
 
-    public void setZipcode(int zipcode) {
+    public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
     }
 
@@ -51,5 +79,4 @@ public class Address {
     public String toString() {
         return street +", "+city+", "+state+", "+zipcode;
     }
-
 }
