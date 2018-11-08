7e5d12d36b6a0c8282b9023768fdb9c567f8c0c2
markdagraca <markdagraca1@gmail.com>
2018-11-08 14:32:13 -0800

    Reverting commits

---
+++ Code/OCRmodule/src/OcrModule.java
@@ -0,0 +1,13 @@
+import refrenceCode.Profile;
+
+public class OcrModule {
+
+    public static void main(String[] args) {
+        Profile profile=new Profile();
+        profile.setFirstName("Mark ");
+
+
+    }
+
+
+}
