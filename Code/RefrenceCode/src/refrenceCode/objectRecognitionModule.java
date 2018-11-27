c8a8f3ebee3c3a39f26a4e51c2118e36032f19bf
markdagraca <markdagraca1@gmail.com>
2018-11-26 19:27:47 -0800

    change ORM module return type

--- Code/RefrenceCode/src/refrenceCode/objectRecognitionModule.java
+++ Code/RefrenceCode/src/refrenceCode/objectRecognitionModule.java
@@ -1,7 +1,6 @@
 package refrenceCode;
 
 import java.io.File;
-import java.util.ArrayList;
 
 public interface objectRecognitionModule {
     /**
@@ -9,5 +8,6 @@ public interface objectRecognitionModule {
      * @param image
      * @return returns a cropped section of any license plate found within an image
      */
-    public ArrayList<File> findLicencePlateinImage(File image);
+    public File findLicencePlateinImage(File image);
+
 }
